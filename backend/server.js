dc512f8b9555f6b91c8ae7fdb2fad64b8bdb139b
Mayankmehta12 <bloggersaabji@gmail.com>
2026-03-26 15:13:46 +0530

    fix port for railway

--- backend/server.js
+++ backend/server.js
@@ -137,6 +137,6 @@ app.use((err, req, res, next) => {
 // ===============================
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`🔥 Server running on port ${PORT}`);
 });
